8aff062f955fb49c2bf6d0b39bdd143443e2dc9d
Msiza-VL <lindokehlemsiza.00@gmail.com>
2025-01-06 17:38:45 +0200

    Implemented user authentication system for Django blog

--- django_blog/forms.py
+++ django_blog/forms.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'email', 'password1', 'password2')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 ]
