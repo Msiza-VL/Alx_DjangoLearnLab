211bd26d0ad6619a6507eab05dcdd1171b3a0d8e
Msiza-VL <lindokehlemsiza.00@gmail.com>
2025-01-06 17:05:59 +0200

    Implemented user authentication system for Django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+TEMPLATES = [
+    {
+        ...
+        'DIRS': [BASE_DIR / 'blog/templates'],
+        ...
+    },
+]
--- django_blog/views.py
+++ django_blog/views.py
@@ -1,1 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def profile(request):
+    return render(request, 'profile.html', {'user': request.user})
